3a8670143fa36ac1972594719487b074786345a8
Subray <61550293+XSubrayX@users.noreply.github.com>
2020-07-20 16:36:28 +0300

    RMX1971: Stay Fluid

---
+++ fluid_rmx1971.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2020 The fluidOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from ysl device
+$(call inherit-product, device/xiaomi/ysl/device.mk)
+
+# Inherit some common fluid stuff.
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+
+# flags for fluid gapps variant
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ysl
+PRODUCT_NAME := fluid_ysl
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi S2
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ysl-user 9 PKQ1.181203.001 V11.0.3.0.PEFMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
